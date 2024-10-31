187ba0c3f2e49a4bda91c2bc14c4d7aa3caad144
Alooysta <guilhermessantos2011@gmail.com>
2024-10-31 16:23:36 -0300

    agora vai

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Landing from "./componentes/Landing";
+import Landing from "./componentes/landing";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
