42a029abf81ba38848369f3f88cf1bd68dec493a
Alooysta <guilhermessantos2011@gmail.com>
2024-11-07 21:56:24 -0300

    agora vai 4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Landing from "./componentes/landing";
+import Landing from "./componentes/landing.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Login from "./componentes/login";
-import Cadastro from "./componentes/cadastro";
-import Dados from "./componentes/dados";
+import Login from "./componentes/login.jsx";
+import Cadastro from "./componentes/cadastro.jsx";
+import Dados from "./componentes/dados.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
