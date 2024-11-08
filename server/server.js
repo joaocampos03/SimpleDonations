3fb09e653f1d683fd839a9741629ee9ae96e986a
Joao Campos <joaomarcelo_1000@hotmail.com>
2024-11-08 19:44:53 -0300

    Criação endpoints atualizaStatus e doacao

--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ async function getNextSequenceValue(sequenceName) {
     return result.seq
 }
 
+//Endpoint registra doação
 app.post('/registrarDoacao', async (req, res) => {
     try {
       const customId = await getNextSequenceValue('produtoid')
@@ -51,6 +52,7 @@ app.post('/registrarDoacao', async (req, res) => {
     }
 })
 
+//Endpoint lista todos as doacoes
 app.get('/doacoes', async (req, res) => {
     try {
       const produtos = await prisma.produtos.findMany()
@@ -61,7 +63,51 @@ app.get('/doacoes', async (req, res) => {
     }
 })
 
-app.delete('/doacoes/:custom_id', async (req, res) => {
+//Endpoint lista somente uma doação específica
+app.get('/doacao/:custom_id', async (req, res) => {
+  try {
+    const customId = parseInt(req.params.custom_id, 10)
+
+    const produto = await prisma.produtos.findUnique({
+      where: {
+        custom_id: customId,
+      }
+    })
+
+    if (produto) {
+      res.status(200).json(produto)
+    } else {
+      res.status(404).json({ message: 'Produto não encontrado!'})
+    }
+  } catch (error) {
+    console.error('Erro ao consultar informações do produto:', error)
+    res.status(500).json({ error: 'Erro ao consultar informações do produto', detalhes: error.message})
+  }
+})
+
+//Endpoint atualiza status da doação
+app.put('/atualizaStatus/:custom_id', async (req, res) => {
+  try{
+    const customId = parseInt(req.params.custom_id, 10)
+
+    await prisma.produtos.update({
+      where: {
+        custom_id: customId,
+      },
+      data: {
+        status: req.body.status
+      }
+    })
+
+    res.status(200).json({ message: 'Status do produto atualizado com sucesso!', data: req.body})
+  } catch (error) {
+    console.error('Erro ao atualizar o status do produto:', error)
+    res.status(500).json({ error: 'Erro ao atualizar o status do produto', detalhes: error.message})
+  }
+})
+
+//Endpoint exclui uma doação
+app.delete('/deletaProduto/:custom_id', async (req, res) => {
   try {
     const customId = parseInt(req.params.custom_id, 10)
 
@@ -70,7 +116,7 @@ app.delete('/doacoes/:custom_id', async (req, res) => {
         custom_id: customId,
       },
     })
-    res.status(200).json({ messsage: 'Doação deletada com sucesso!'})
+    res.status(200).json({ messsage: 'Doação deletada com sucesso!', data: req.body})
   } catch (error) {
     console.error('Erro ao deletar doação:', error)
     res.status(500).json({ error: 'Erro ao deletar doação', detalhes: error.message})
