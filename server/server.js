3c684099be39f62bd15fb4dc77b6ada1bf0a32a0
Joao Campos <joaomarcelo_1000@hotmail.com>
2024-11-12 20:27:19 -0300

    Upando o cloudinary e o cors

--- server/server.js
+++ server/server.js
@@ -4,9 +4,15 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const prisma = new PrismaClient()
 const app = express()
+app.use(cors());
 app.use(express.json())
 
 mongoose.connect(process.env.DATABASE_URL)
